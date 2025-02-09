b24000c72fc5e2923fde605289a653e2e1144ee4
Prince Edouard <pedouard2@gmail.com>
2025-02-09 14:31:41 -0800

    template code

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,2 @@
 from abc import ABC, abstractmethod
 from typing import List, Dict, Optional
-
-
-class JobAPI(ABC):
-    @abstractmethod
-    def search_jobs(
-        self, search_terms: Optional[str], location: Optional[str]
-    ) -> List[Dict]:
-        pass
